f26abd41c358e8b2e7e4faa15ae107acd1f27a91
Smit Rao <rao.smit.2@gmail.com>
2018-01-26 22:18:54 -0500

    Added game_functions and generic_functions

---
+++ generic_functions.py
@@ -0,0 +1,33 @@
+'''Useful Generic Functions'''
+
+# This is the intellectual property of Smit Rao (developer). 
+# Please request permission to use the following functions at raosmit2@gmail.com
+
+
+import doctest
+import random
+import time
+from typing import List, Dict, Tuple
+
+
+
+
+def capitalize_first(chars: str) -> str:
+    '''Return chars capitalized (first letter only).
+    
+    >>> capitalize_first('asdlfjsl asdfji3 4 4f5 oa vasuvoia234ov')
+    'Asdlfjsl asdfji3 4 4f5 oa vasuvoia234ov'
+    >>> capitalize_first('
+    '''
+    
+    
+def capitalize_words(chars: str) -> str:
+    '''
+    '''
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    doctest.testmod()
